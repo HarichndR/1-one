4ef4d1b725e960b152c15d110da2a596525f2276
HarichndR <114823957+HarichndR@users.noreply.github.com>
2025-12-28 19:21:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: process.env.ALLOWED_CORS_ORGIN_URL,
       methods: ['GET', 'POST'],
       credentials:true,
     }
